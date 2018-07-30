69e1b9ab7c8280c60fbd350daa20da38187f6d77
8ayac <8ayac.y@gmail.com>
2018-07-31 01:02:52 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	n, err := strconv.Atoi(r.URL.Path[10:])
+	if err != nil {
+		invalidNumber(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "%q", fizzbuzz(n))
+}
+
+func fizzbuzz(n int) (result string) {
+	if n%3 == 0 {
+		result += "Fizz"
+	}
+	if n%5 == 0 {
+		result += "Buzz"
+	}
+	if len(result) == 0 {
+		result += strconv.Itoa(n)
+	}
+	return
+}
+
+func invalidNumber(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte("Invalid number."))
+}
+
+func main() {
+	http.HandleFunc("/fizzbuzz/", viewHandler)
+	http.ListenAndServe(":8080", nil)
+}
